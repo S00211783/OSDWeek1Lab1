a6df4ee499a42012d4fc5ba0647c1cf185788f9d
Matthew Blain <53818917+MatthewBlain02@users.noreply.github.com>
2024-01-24 22:52:59 +0000

    Pushing Code

--- src/strings/greet.spec.ts
+++ src/strings/greet.spec.ts
@@ -1,7 +1,7 @@
 import { greet } from "./greet";
 
 describe('greet', () => {
-    it('it should include Matthew Blain in the message', () => {
-        expect(greet('Matthew Blain')).toContain('Matthew Blain');
+    it('it should include Matthew Blain S00211783 in the message', () => {
+        expect(greet('Matthew Blain S0021783')).toContain('Matthew Blain S0021783');
     });
 });
